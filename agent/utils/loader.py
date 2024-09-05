586ef0a27a7945d5a632890a3efe89920c348d07
ariete <deningliu@163.com>
2024-09-05 15:40:23 +0800

    feat(loader): 添加load cot prompt功能
    将load prompt功能移植到loader中

--- agent/utils/loader.py
+++ agent/utils/loader.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset
+from datasets import load_dataset, DatasetDict
 from langchain_core.prompts import ChatPromptTemplate
 from langchain import hub
 from dotenv import load_dotenv, find_dotenv
@@ -13,6 +13,35 @@ DATASET_NAME = [
 	"tabmwp",
 	"svamp",
 ]
+
+
+def process_dataset(file_path: str) -> DatasetDict:
+	# Load dataset
+	dataset = load_dataset("json", data_files=file_path, split="train")
+	if "tabmwp" in file_path:
+		updated_dataset: DatasetDict = dataset.map(
+			lambda example: {
+				"context": f"Read the following table regarding {example["table_title"]} and then use \
+				tools to get the final answer\n{example["table"]}\n",
+				"question": example["question"],
+				"answer": example["answer"]})
+	elif "gsm8k" in file_path:
+		updated_dataset: DatasetDict = dataset.map(
+			lambda example: {
+				"context": "Read the following question and then use tools to get the final answer\n",
+				"question": example["question"],
+				"answer": example["answer"]
+			})
+	elif "svamp" in file_path:
+		updated_dataset: DatasetDict = dataset.map(
+			lambda example: {
+				"context": f"Read the following text and then use tools to get the final answer\n{example['Body']}\n",
+				"question": example["Question"],
+				"answer": example["Answer"]
+			})
+	else:
+		raise ValueError("Dataset not supported")
+	return updated_dataset
 
 
 # Load prompt
@@ -37,6 +66,8 @@ def main():
 	# Load dataset
 	prompt = load_prompt(dataset_name="hotpot_qa")
 	prompt.pretty_print()
+	dataset = process_dataset(file_path="/Users/ariete/Projects/self-improve/data/tabmwp.jsonl")
+	print(dataset)
 
 
 if __name__ == '__main__':
